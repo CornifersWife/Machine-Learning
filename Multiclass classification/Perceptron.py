3c9286b4eb3ab9712ca5db0578f22129635e4ea9
CornifersWife <irys.latosz@gmail.com>
2024-04-21 22:39:25 +0200

    finished LogisticRegressive

--- Multiclass classification/Perceptron.py
+++ Multiclass classification/Perceptron.py
@@ -1,29 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
 from Plot import plot_decision_regions
 
 
 class Perceptron(object):
 
-    def __init__(self, eta=0.01, n_iter=10, random_state=1):
-        self.errors_ = None
-        self.w_ = None
+    def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
 
     def fit(self, X, y):
         self.w_ = np.zeros(1 + X.shape[1])
-        self.errors_ = []
+
         for _ in range(self.n_iter):
-            errors = 0
+            # errors = 0
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
         return self
 
     def net_input(self, X):
@@ -40,32 +37,23 @@ def main():
     print(X)
     y = iris.target
     print(y)
-    class_labels = iris.target_names
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
-    # X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
-    # y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+    # X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)] y_train_01_subset = y_train[(y_train == 0) | (
+    # y_train == 1)]
     # w perceptronie wyjście jest albo 1 albo -1
     # y_train_01_subset[(y_train_01_subset == 0)] = -1
-
-    # y_train[y_train != 2] = -1
-    # y_train[y_train == 2] = 1
-    # print(y_train)
-
-    classifiers = []
-    for class_idx in np.unique(y_train):
-        ppn = Perceptron(eta=0.01, n_iter=50, random_state=1)
-        binary_y_train = np.where(y_train == class_idx, 1, -1)  # Prepare binary target vector
-        ppn.fit(X_train, binary_y_train)
-        classifiers.append(ppn)
+    y_train[y_train != 2] = -1
+    y_train[y_train == 2] = 1
+    print(y_train)
 
     ppn = Perceptron(eta=0.1, n_iter=1000)
     ppn.fit(X_train, y_train)
 
-    plot_decision_regions(X=X_train, y=y_train, classifier=classifiers[-1],class_labels=class_labels)
-    plt.xlabel('Petal length')
-    plt.ylabel('Petal width')
+    plot_decision_regions(X=X_train, y=y_train, classifier=ppn)
+    plt.xlabel(r'$x_1$')
+    plt.ylabel(r'$x_2$')
     plt.legend(loc='upper left')
     plt.show()
 
--- Multiclass classification/Reglog.py
+++ Multiclass classification/Reglog.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.special import softmax
+from scipy.special import softmax #TODO ask if i can just do it like this
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
@@ -12,26 +12,26 @@ def activation(z):
 
 
 class LogisticRegressionGradientDescent:
-    def __init__(self, eta=0.05, n_iter=100, random_state=1, num_classes=3):
-        self.w_ = None
+    def __init__(self, eta=0.05, number_of_iterations=100, random_state=1, number_of_classes=3):
+        self.weights = None
         self.eta = eta
-        self.n_iter = n_iter
+        self.number_of_iterations = number_of_iterations
         self.random_state = random_state
-        self.num_classes = num_classes
+        self.number_of_classes = number_of_classes
 
     def fit(self, X, y):
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=(X.shape[1] + 1, self.num_classes))
-        for i in range(self.n_iter):
+        random_generation = np.random.RandomState(self.random_state)
+        self.weights = random_generation.normal(scale=0.01, size=(X.shape[1] + 1, self.number_of_classes))
+        for i in range(self.number_of_iterations):
             net_input = self.net_input(X)
             output = activation(net_input)
             errors = y - output
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum(axis=0)
+            self.weights[1:] += self.eta * X.T.dot(errors)
+            self.weights[0] += self.eta * errors.sum(axis=0)
         return self
 
     def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+        return np.dot(X, self.weights[1:]) + self.weights[0]
 
     def predict(self, X):
         # noinspection PyTypeChecker
@@ -43,15 +43,17 @@ def main():
     X = iris.data[:, [2, 3]]
     y = iris.target
     class_labels = iris.target_names
-    num_classes = len(np.unique(y))
+    number_of_classes = len(np.unique(y))
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
-    Y_train_onehot = np.eye(num_classes)[y_train]
+    Y_train_onehot = np.eye(number_of_classes)[y_train]
 
-    logisticRegressionGradientDescent = LogisticRegressionGradientDescent(eta=0.05, n_iter=500, random_state=1, num_classes=num_classes)
+    logisticRegressionGradientDescent = LogisticRegressionGradientDescent(eta=0.05, number_of_iterations=500,
+                                                                          number_of_classes=number_of_classes)
     logisticRegressionGradientDescent.fit(X_train, Y_train_onehot)
 
-    plot_decision_regions(X=X_train, y=y_train, classifier=logisticRegressionGradientDescent, class_labels=class_labels)
+    print(logisticRegressionGradientDescent.weights)
+    plot_decision_regions(X=X_test, y=y_test, classifier=logisticRegressionGradientDescent, class_labels=class_labels)
     plt.xlabel('Petal length')
     plt.ylabel('Petal width')
     plt.legend(loc='upper left')
